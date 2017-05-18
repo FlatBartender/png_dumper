9b2a10c883f3e9afd8587e37c0e6762e4a0af307
FlatBartender <smithseb.nn@gmail.com>
2017-05-18 19:22:57 +0200

    Added some verbosity

--- get_png.py
+++ get_png.py
@@ -2,7 +2,7 @@ import sys;
 import string;
 
 if len(sys.argv) != 2:
-	print("Wrong syntax.\nSyntax: python get_png.py [filename]\n");
+	print("Wrong syntax.\nSyntax: python get_png.py [filename]");
 	exit();
 	
 filename = sys.argv[1];
@@ -15,11 +15,11 @@ png_end = b"IEND";
 
 start = data.find(png_header);
 while start != -1:
-	end = data.find(png_end)+8;
+	end = data.find(png_end, start+8)+8;
+	print("PNG file found between "+hex(start)+" and "+hex(end)+"");
 	pngfile = data[start:end];
-	data = data[end:-1];
 	o = open("img"+str(nb_img)+".png", "wb");
 	o.write(pngfile);
 	o.close();
 	nb_img+=1;
-	start = data.find(png_header);
+	start = data.find(png_header, start+8);
